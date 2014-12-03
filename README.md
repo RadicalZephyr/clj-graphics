91f7f42469bbf3986ba5006b63899e48f0991bdb
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-02 21:12:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# clj-graphics
+
+A clojure library for doing 2D graphics processing.
+
+## License
+
+Copyright © 2014 Geoff Shannon
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-graphics "0.1.0-SNAPSHOT"
+  :description "A clojure library for doing 2D graphics processing"
+  :url "http://www.github.com/RadicalZephyr/clj-graphics"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [seesaw "1.4.4"]])
