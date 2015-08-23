d2feed4ef789c6d2c37179edfb3659d7f020e7f0
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-23 13:23:51 -0500

    Add vectorz as a dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://www.github.com/RadicalZephyr/clj-graphics"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.mikera/core.matrix "0.37.0"]
+                 [net.mikera/vectorz-clj "0.33.0"]
                  [seesaw "1.4.4"]])
